175ca095518d963e5b7af02f654ee91bd1d69788
poblouin <blouin.pierreolivier@gmail.com>
2018-04-08 18:21:17 -0400

    Validate if transaction is assigned to an inactive budget

--- budgetme/apps/transactions/serializers.py
+++ budgetme/apps/transactions/serializers.py
@@ -1,3 +1,5 @@
+import datetime
+
 from rest_framework import serializers
 
 from budgetme.apps.transactions.models import Transaction
@@ -22,7 +24,8 @@ class TransactionSerializer(serializers.ModelSerializer):
         transaction_category_data = validated_data.pop('transaction_category')
         budget_data = transaction_category_data.pop('budget')
 
-        BudgetSerializer.validate_budget_or_raise(user, **budget_data)
+        budget = BudgetSerializer.validate_budget_or_raise(user, **budget_data)
+        TransactionSerializer._budget_is_active_or_raise(budget)
         transaction_category = TransactionCategorySerializer.validate_transaction_category_or_raise(user, transaction_category_data['name'])
 
         return Transaction.objects.create(
@@ -34,6 +37,10 @@ class TransactionSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         user = self.context['request'].user
         transaction_category_data = validated_data.pop('transaction_category')
+        budget_data = transaction_category_data.pop('budget')
+
+        budget = BudgetSerializer.validate_budget_or_raise(user, **budget_data)
+        TransactionSerializer._budget_is_active_or_raise(budget)
         transaction_category = TransactionCategorySerializer.validate_transaction_category_or_raise(user, transaction_category_data['name'])
 
         instance.amount = validated_data.get('amount', instance.amount)
@@ -42,3 +49,12 @@ class TransactionSerializer(serializers.ModelSerializer):
         instance.transaction_category = transaction_category
         instance.save()
         return instance
+
+    @staticmethod
+    def _budget_is_active_or_raise(budget):
+        if not (budget.start_date and budget.end_date):
+            return
+
+        now = datetime.datetime.utcnow().date()
+        if not (budget.start_date <= now <= budget.end_date):
+            raise serializers.ValidationError('Cannot assign a transaction to an inactive budget.')
